d6646c15ee658ed4f30b79757557e2037129ef38
Student <example@example.com>
2019-07-08 15:44:28 -0700

    refactored hands to hold arrOfHands

--- src/Hands.js
+++ src/Hands.js
@@ -1,9 +1,12 @@
 import Card from './Card.js';
+import Hand from './Hand.js'
 
 //Author: Ben Martinson
 
 export default class Hands{
-  constructor(){
+  constructor(arrOfHands){
+
+    this.arrOfHands = [new Hand([])];
 
     this.findFullHouse = this.findFullHouse.bind(this);
     this.find4Kind = this.find4Kind.bind(this);
@@ -14,59 +17,39 @@ export default class Hands{
 
     this.hands = [[this.findFullHouse, "Full House"],[this.find4Kind, "4 of a Kind"] ,[this.find3Pair, "3 of a Kind"],
                   [this.find2Pair, "Two Pair"], [this.findPair, "Pair"], [this.findHighCard, "High Card"]];
-    this.cards = []
-    this.cards.push(new Card('4', 'heart'));
-    this.cards.push(new Card('7', 'heart'));
-    this.cards.push(new Card('6', 'heart'));
-    this.cards.push(new Card('5', 'heart'));
-    this.cards.push(new Card('2', 'heart'));
 
-    this.bestHand;
-    this.counts = []
-    this.message = "None";
-    this.getCounts();
-    this.findBestHand();
-    console.log(this.message, this.bestHand);
+
+    this.findBestHands();
   }
 
-  getCounts(){
-    for (var i = 0; i < 13; i++) {
-      this.counts.push(0);
-    }
-    console.log(this.counts);
-    this.cards.forEach(function(card, idx){
-      this.counts[card.value]++
-    }, this)
-    console.log(this.counts);
-  }
 
-  findBestHand(){
+  findBestHand(hand){
     for (let i = 0; i < this.hands.length; i++) {
-      let hand = this.hands[i];
-      let result = hand[0](this.cards);
-      this.message = hand[1];
+      let result = this.hands[i][0](hand);
+
+      hand.message = this.hands[i][1];
       if(result.length !== 0){
-        this.bestHand = result;
+        hand.bestHand = result;
         return;
       }
     }
   }
 
-  findPair(){
-    for (let i = 0; i < this.counts.length; i++) {
-      if(this.counts[i] === 2){
+  findPair(hand){
+    for (let i = 0; i < hand.counts.length; i++) {
+      if(hand.counts[i] === 2){
         return [i, i];
       }
     }
     return [];
   }
 
-  find2Pair(){
+  find2Pair(hand){
     let firstPair;
     let secondPair;
 
-    for (let i = 0; i < this.counts.length; i++) {
-      if(this.counts[i] === 2){
+    for (let i = 0; i < hand.counts.length; i++) {
+      if(hand.counts[i] === 2){
         if(firstPair){
           return [firstPair, [i, i]];
         } else {
@@ -77,18 +60,18 @@ export default class Hands{
     return [];
   }
 
-  find3Pair(){
-    for (var i = 0; i < this.counts.length; i++) {
-      if(this.counts[i] === 3){
+  find3Pair(hand){
+    for (var i = 0; i < hand.counts.length; i++) {
+      if(hand.counts[i] === 3){
         return [i, i, i];
       }
     }
     return [];
   }
 
-  find4Kind(){
-    for (var i = 0; i < this.counts.length; i++) {
-      if(this.counts[i] === 4){
+  find4Kind(hand){
+    for (var i = 0; i < hand.counts.length; i++) {
+      if(hand.counts[i] === 4){
         return [i, i, i, i];
       }
     }
@@ -96,106 +79,31 @@ export default class Hands{
   }
 
 
-  findBestHands(cards1, cards2){}
+  findBestHands(){
+    let handResults = []
+    this.arrOfHands.forEach(function(hand){
+      this.findBestHand(hand);
+    }, this)
+    console.log(this.arrOfHands);
+  }
 
 
-  findFullHouse(cards){
-    let threePair = this.find3Pair();
-    let pair = this.findPair();
+  findFullHouse(hand){
+    let threePair = this.find3Pair(hand);
+    let pair = this.findPair(hand);
     console.log(threePair, pair);
     if(threePair.length && pair.length)
       return [threePair, pair];
     return [];
   }
 
-  // const findFullHouse = (cards) => {
-  //   let threePair = this.find3Pair();
-  //   let pair = this.findPair();
-  //   console.log(this);
-  //   if(threePair && pair)
-  //     return [threePair, pair];
-  //   return [];
-  // }
 
-  findHighCard(cards){
-    for (var i = this.counts.length; i > 0; i--) {
-      if(this.counts[i] > 0)
+  findHighCard(hand){
+    for (var i = hand.counts.length; i > 0; i--) {
+      if(hand.counts[i] > 0)
         return i;
     }
     return [];
   }
 
-
-
-
 }
-
-
-
-
-
-
-// find2Pair(cards, that){
-//   let pairs = that.findPairs(cards);
-//   console.log(pairs);
-//   if(pairs.length === 4)
-//     return pairs
-//   return [];
-// }
-//
-// findPairs(cards){
-//   let tempCards = [...cards];
-//   let pairs = []
-//
-//   for (let i = 0; i < tempCards.length; i++) {
-//     for (var x = i+1; x < tempCards.length; x++) {
-//       if(tempCards[i].value === tempCards[x].value){
-//         //pair found
-//         pairs.push(tempCards[i]);
-//         pairs.push(tempCards[x]);
-//         tempCards.splice(x, 1)
-//         tempCards.splice(i, 1)
-//       }
-//     }
-//   }
-//   return pairs;
-// }
-//
-// find4Kind(cards){
-//   let pairs = [];
-//   for (let i = 0; i < cards.length; i++) {
-//     for (let x = i+1; x < cards.length; x++) {
-//       for (let r = x+1; r < cards.length; r++) {
-//         for (let l = r+1; l < cards.length; l++) {
-//           if(cards[i].value === cards[x].value && cards[i].value === cards[r].value && cards[i].value === cards[l].value){
-//             //pair found
-//             pairs.push(cards[i])
-//             pairs.push(cards[x])
-//             pairs.push(cards[r])
-//             pairs.push(cards[l])
-//           }
-//         }
-//       }
-//     }
-//   }
-//   return pairs;
-//
-// }
-//
-//
-// find3Pair(cards){
-//   let pairs = [];
-//   for (let i = 0; i < cards.length; i++) {
-  //     for (let x = i+1; x < cards.length; x++) {
-    //       for (let r = x+1; r < cards.length; r++) {
-      //         if(cards[i].value === cards[x].value && cards[i].value === cards[r].value){
-        //           //pair found
-        //           pairs.push(cards[i])
-        //           pairs.push(cards[x])
-        //           pairs.push(cards[r])
-        //         }
-        //       }
-        //     }
-        //   }
-        //   return pairs;
-        // }
