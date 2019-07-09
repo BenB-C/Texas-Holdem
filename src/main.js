b1e07232ff8382662c6b379ee4487c5e199213ee
Ben Bennett-Cauchon and Josh Horgen <Guest@Epicodus-8D.local>
2019-07-09 09:27:50 -0700

    Removed loop to focus on just one round to start

--- src/main.js
+++ src/main.js
@@ -5,29 +5,30 @@ import {Deck} from './Deck.js';
 import Hands from './Hands.js';
 import Player from './players.js';
 
-
-
-let numberOfPlayers = 4;
+// Create players
+let numberOfPlayers = 2;
 let players = [];
 for (let i = 0; i < numberOfPlayers; i++) {
   players.push(new Player());
 }
+// Layout table
+
+
+// Start game
 let game = new Game(players);
 game.dealCards(2, true);
 game.players.forEach((player, i) => {
   console.log("Player " + i + " was dealt:" + player.hand);
 });
-let gameOver  = false;
-while (!gameOver) {
-  game.takeBets();
-  game.dealCards(3);
-  game.takeBets();
-  game.dealCards(1);
-  game.takeBets();
-  game.dealCards(1);
-  game.takeBets();
-  gameOver = true;
-}
+
+game.takeBets();
+game.dealCards(3);
+game.takeBets();
+game.dealCards(1);
+game.takeBets();
+game.dealCards(1);
+game.takeBets();
+
 $(document).ready(function(){
 
 })
