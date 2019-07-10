4173c113f3122df92767a47acb4ef2b3839aa398
Ben Bennett-Cauchon and Josh Horgen <Guest@Epicodus-8D.local>
2019-07-10 09:55:38 -0700

    the button display is now working

--- src/Game.js
+++ src/Game.js
@@ -11,7 +11,8 @@ export class Game {
     this.betsNeeded = players.length;
     this.communityCards = [];
     this.dealerIndex = this.players.length - 1;
-    this.currentlyBettingIndex = (this.dealerIndex + 1) % this.players.length;
+    // this.currentlyBettingIndex = (this.dealerIndex + 1) % this.players.length;
+    this.currentlyBettingIndex = 0;
   }
 
   dealCards(numberOfCards, isFirstDeal) {
@@ -46,13 +47,20 @@ export class Game {
 
   incTurn(){
     this.currentlyBettingIndex = (this.currentlyBettingIndex + 1) % this.players.length;
+    console.log('idx', this.currentlyBettingIndex);
     if(this.betsNeeded > 0){
       if (!this.players[this.currentlyBettingIndex].hasFolded){
         this.betsNeeded--
-        this.takeBet(this.currentlyBettingIndex);
+        if(this.players[this.currentlyBettingIndex].isComputer){
+
+          return "computerTurn";
+        }else {
+          return "userTurn";
+        }
       }
+      this.incTurn();
     } else{
-      this.resetBetting();
+      return "reset";
     }
   }
 
@@ -63,22 +71,19 @@ export class Game {
     })
   }
 
-  takeBet(playerIdx) {
-    //$(".display-bet-buttons").hide();
-    if(!this.players[playerIdx].hasFolded){
-      if (this.players[playerIdx].isComputer) {
-        setTimeout(() => {
-          console.log("computer-turn");
-          this.incTurn();
-
-        }, 1500)
-      } else {
-        //$(".display-bet-buttons").show();
-      }
-    }
-    console.log('pot', this.pot);
-    console.log('currentBet', this.currentBet);
-  }
+  // takeBet(playerIdx) {
+  //   //$(".display-bet-buttons").hide();
+  //   if(!this.players[playerIdx].hasFolded){
+  //     if (this.players[playerIdx].isComputer) {
+  //       this.currentPlayerComp = true;
+  //     } else {
+  //       this.currentPlayerComp = false;
+  //       //$(".display-bet-buttons").show();
+  //     }
+  //   }
+  //   console.log('pot', this.pot);
+  //   console.log('currentBet', this.currentBet);
+  // }
 
   handleFold(){
     console.log("player has folded");
@@ -99,7 +104,7 @@ export class Game {
     amount = 100;
     let stillIn = 0;
     // this.players.forEach(function())
-    this.betsNeeded = this.players.length-1;
+    this.betsNeeded = this.players.length;
     console.log('bet Placed', this.currentBet+amount);
     let bettingPlayer = this.players[this.currentlyBettingIndex];
     let amountToCall = this.currentBet - bettingPlayer.bet;
